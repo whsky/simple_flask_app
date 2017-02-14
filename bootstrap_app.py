922a5468017d20804f066262337cbae626f90f2c
Steve Iannaccone <renegadeoffunk1200@yahoo.com>
2017-02-14 15:51:19 -0700

    Changed port number and added threaded=True to app.py file

--- bootstrap_app.py
+++ bootstrap_app.py
@@ -7,4 +7,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8105, threaded=True)
